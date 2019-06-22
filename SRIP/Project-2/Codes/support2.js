2a7467c658693c1f24231b2677a15a487bd3923a
Mudit Gupta <muditgupta9807@gmail.com>
2019-06-22 12:30:10 +0530

    Taking all inputs properly with alert system for wrong inputs

--- SRIP/Project-2/Codes/support2.js
+++ SRIP/Project-2/Codes/support2.js
@@ -23,9 +23,10 @@ var polyside;//For Sides Of the Polygon
 var polyco;//For storing the coordinates in Text Form
 var polygonPath=[];//For Storing actual Coordinates;
 var lineRecord=[];
-//var coordinates=[[2,2],[2,8],[8,8],[8,2],[10,10],[12,13],[14,15],[16,17],[18,19],[20,21]];//temproray storage of coordinates
-var coordinates=[[50,200],[200,50],[350,200],[200,350],[360,150],[320,290],[250,380],[200,420],[150,250],[100,210]];
+var coordinates=[[2,2],[2,8],[8,8],[8,2],[10,10],[12,13],[14,15],[16,17],[18,19],[20,21]];//temporary storage of coordinates
 
+//For Informing Initial Function If all Values are correct and within the range or not
+var wrongInputs=0;
 
 function update() 
 {
@@ -41,26 +42,128 @@ function update()
 	//Clearing all back values
 	polygonPath=[];
 	lineRecord=[];
+
+	//Clearing the Printing Area
+	p1.innerHTML="Updates The Values ";
+	p2.innerHTML="Now Start The Experiment";
+	p3.innerHTML="";
+	p4.innerHTML="";
+	p5.innerHTML="";
+	p6.innerHTML="";
+	p7.innerHTML="";
+	p8.innerHTML="";
+	p9.innerHTML="";
+	p0.innerHTML="";
+
+	
+
+	//Alert Conditions On entering wrong values by the user 
+	var alreadyChecked=1;
+
+	//Checking For the Wrong Inputs Given By the User
+	if(width>25 || height>25 || width<1 || height<1)//for correct frame values 
+	{
+		p1.innerHTML="";
+		p2.innerHTML="";
+		alert("For Frame Width and Height\nMin. Value is 1\nMax. value is 25\nRe-enter the Values and Update");
+		wrongInputs =1;
+		alreadyChecked=0;
+	}
+	if(alreadyChecked)//for min. and max. polygon coordinate values
+	{
+		var p=0;var even=0;
+		for(var q=0;q<polyco.length;q++)
+		{
+			if(polyco[q]== " " || polyco[q]== ",")
+			{
+				var t = Number(polyco.substring(p,q));
+				p=q+1;
+				if(t<0 || (even%2==0 && t>width) || (even%2!=0 && t>height))
+				{
+					p1.innerHTML="";
+					p2.innerHTML="";
+					alert("Polygon \nMin. value of coordinates (0,0)\nMax. value of coordinates ("+width+","+height+")\nRe-enter the Values and Update");
+					wrongInputs=1;
+					break;
+				}
+				even++;
+			}
+		}
+	}
+	alreadyChecked=1;
+	if(polyside<3 || polyside>10)//for correct polygon sides
+	{
+		p1.innerHTML="";
+		p2.innerHTML="";
+		alert("Min. Value of Polygon Sides is 3 \nMax. Value of Polygon Sides is 10\nRe-enter the Values and Update");
+		wrongInputs=1;
+		alreadyChecked=0;
+
+	}
+	var commaCount =0;
+	var spaceCount =0;
+	for (var i = 0; i < polyco.length; i++) //for correct order of inputs in textarea
+	{
+		if(polyco[i] == "," && commaCount<polyside )
+			commaCount++;
+		if(polyco[i] == " " && commaCount<polyside)
+			spaceCount++;
+		if(isNaN(polyco[i]) && (polyco[i]!=" ") && (polyco[i]!=","))
+		{
+			p1.innerHTML="";
+			p2.innerHTML="";
+			alert("Polygon Coordinates Entered Wrong\nEnter the Coordinates in format - x1 y1,x2 y2,x3 y3,\nRe-enter the Values and Update");
+			wrongInputs=1;
+			alreadyChecked=0;
+			break;
+		}	
+	}
+	if(alreadyChecked)	
+	{
+		if(commaCount<polyside)
+		{
+			p1.innerHTML="";
+			p2.innerHTML="";
+			alert("Enter the Remaining Coordinates and Update");
+			wrongInputs=1;
+		}
+		else if(spaceCount!=polyside)
+		{
+			p1.innerHTML="";
+			p2.innerHTML="";
+			alert("Remove The Extra Spaces\nEnter the Coordinates in format - x1 y1,x2 y2,x3 y3,\nRe-enter the Values and Update");
+			wrongInputs=1;
+		}
+	}
+	
 }
 
 function initial()
 {
-	/*update();
-	var x=0,y=0;
-	var j=0,i=0;
-	for(i=0;i<polyco.length;i++)
+	update();
+	if(wrongInputs)
 	{
-		if(polyco[i]== " " || polyco[i]== ",")
+	}
+	else
+	{
+		p1.innerHTML="Experiment to begin soon";
+		p2.innerHTML="";
+		var x=0,y=0;
+		var j=0,i=0;
+		for(i=0;i<polyco.length;i++)
 		{
-			coordinates[x][y]=Number(polyco.substring(j,i));
-			y++;
-			j=i+1;
-			if(polyco[i] == ",")
+			if(polyco[i]== " " || polyco[i]== ",")
 			{
-				x++;
-				y=0;
+				coordinates[x][y]=Number(polyco.substring(j,i));
+				y++;
+				j=i+1;
+				if(polyco[i] == ",")
+				{
+					x++;
+					y=0;
+				}
 			}
 		}
-	}*/
-	alert(coordinates);
+	}
+
 }
